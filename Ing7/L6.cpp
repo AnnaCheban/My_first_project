9a67dc293d0ae06201e902147b1bd07c7e286f96
AnnaCheban <aceban098@gmail.com>
2021-11-10 23:51:05 +0200

    new changes

--- Ing7/L6.cpp
+++ Ing7/L6.cpp
@@ -10,14 +10,13 @@ int main()
 	float h;
 	cout << "Enter a =";
 	cin >> a;
-	cout << "Enter b =";
-	cin >> b;
+	
 	cout << "Enter h =";
 	cin >> h;
 	
 	float y, x = a;
 	do {
-		y = pow(sin(x), 2) + 2 * cos(x) + 3;
+		y = pow(sin(x), 2) + 2 * cos(x) + 3;    //коментар
 		cout << setw(10) << "x=" << x << setw(10) << "y=" << y << endl;
 		x += h;
 	} while (x <= b);
